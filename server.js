50d157d0e2ad116b1b785db1dccca67ab68feb8d
Harshvardhan sharma <115737140+Harshvardhan77@users.noreply.github.com>
2023-12-02 11:18:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const db= knex({
     connection:{ 
         host:'127.0.0.1',
         user:'postgres',
-        password:'impossible2002',
+        password:'',
         database:'signup',
     },
 });
